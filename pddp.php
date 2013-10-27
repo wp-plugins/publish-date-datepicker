1072d72d1609eac533a6af5f93435a7e6a338d56
vinod dalvi <vinod dalvi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-27 05:13:42 +0000

    Tagging version 2.0.0
    git-svn-id: https://plugins.svn.wordpress.org/publish-date-datepicker/tags/2.0.0@794319 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- pddp.php
+++ pddp.php
@@ -1,28 +1,29 @@
 <?php
 /*
 Plugin Name: Publish Date DatePicker
+Plugin URI: http://www.cupeltech.com/
 Description: Publish Date DatePicker adds interactive calendar in publish section of post, page & custom post.
 It makes adding and changing date easier by selecting it from interactive calendar using mouse.
-Version: 1.0.0
+Version: 2.0.0
 Author: Vinod Dalvi
-Author URI: http://profiles.wordpress.org/vinod-dalvi
+Author URI: http://www.cupeltech.com/
 License: GPLv2 or later
 */
 
-/*  Copyright YEAR  PLUGIN_AUTHOR_NAME  (email : PLUGIN AUTHOR EMAIL)
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as
-    published by the Free Software Foundation.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 // Make sure we don't expose any info if called directly
@@ -31,14 +32,15 @@ if ( !function_exists( 'add_action' ) ) {
 	exit;
 }
 
-define('PDDP_VERSION', '1.0.0');
+define('PDDP_VERSION', '2.0.0');
 define('PDDP_PLUGIN_URL', plugin_dir_url( __FILE__ ));
 
 function load_pddp_admin_style_script($hook) {
-         if( 'post.php' != $hook && 'post-new.php' != $hook)
-            return;
+	if( 'post.php' != $hook && 'post-new.php' != $hook)
+		return;
 
-     wp_enqueue_script('pddp_script',PDDP_PLUGIN_URL . 'js/pddp.js',array( 'jquery-ui-datepicker' ));
-     wp_enqueue_style('pddp_css',PDDP_PLUGIN_URL . 'css/pddp.css');
+	wp_enqueue_script( 'timepicker-js', PDDP_PLUGIN_URL . 'js/jquery-ui-timepicker-addon.js', array( 'jquery-ui-datepicker' ) );
+	wp_enqueue_script( 'pddp-js', PDDP_PLUGIN_URL . 'js/pddp.js', array( 'timepicker-js' ) );
+	wp_enqueue_style( 'pddp-css', PDDP_PLUGIN_URL . 'css/pddp.css' );
 }
 add_action( 'admin_enqueue_scripts', 'load_pddp_admin_style_script' );
